3dd4a744dcb09f4e18d56054fef007773d8627e5
0xwhiz <netdragon986@gmail.com>
2025-07-10 19:45:06 +0000

    feat: add cors

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const apodRoute = require('../routes/apod');
 const marsRoute = require('../routes/mars');
 const epicRoute = require('../routes/epic');
@@ -8,6 +9,13 @@ const imageLibraryRoute = require('../routes/imageLibrary');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(cors({
+  origin: ['https://bounce-nasa-space-explorer.vercel.app/', 'http://localhost:8001'], // Add your frontend URLs here
+  credentials: true // if you need to send cookies/auth headers
+}));
+
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/apod', apodRoute);
